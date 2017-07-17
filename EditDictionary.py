e432d1fd6918915da7aa1fc0d93f047f7739d364
Duluri\Ryan <ryanoftinellb@gmail.com>
2017-07-18 04:10:40 +1000

    Refactor: rearrange for greater readability of code; change double quotes to single quotes where able.

--- EditDictionary.py
+++ EditDictionary.py
@@ -14,60 +14,64 @@ class EditDictionary(Tk.Frame):
         :param searchfile (String): the path, filename and extension of the file wherein the json text for site searches will be placed, relative to dir
         """
         Tk.Frame.__init__(self, master)
+        os.chdir(dir)
+        # initialise instance variables
+        self.site = site
+        self.outputfile = outputfile
+        self.searchfile = searchfile
+        self.random_word = Tk.StringVar()
+        self.language = Tk.StringVar()
+        self.entry = ''
+        self.page = None
+        # initialise other useful classes. Default language is High Lulani
+        self.markdown = Markdown()
+        self.translator = Translator('hl')
+        # initialise textboxes and buttons
         self.heading = None
         self.go_button = None
         self.publish_button = None
         self.edit_text = None
         self.random_words = None
-        self.random_word = Tk.StringVar()
-        self.language = Tk.StringVar()
-        self.high_lulani = Tk.Radiobutton(self, text='High Lulani', value="hl", variable=self.language,
+        self.high_lulani = Tk.Radiobutton(self, text='High Lulani', value='hl', variable=self.language,
                                           command=self.change_language)
-        self.english = Tk.Radiobutton(self, text='English', value="en", variable=self.language,
+        self.english = Tk.Radiobutton(self, text='English', value='en', variable=self.language,
                                       command=self.change_language, anchor=Tk.N)
         self.high_lulani.select()
-        self.entry = ""
-        self.page = None
-        self.is_bold = False
-        self.is_italic = False
-        self.is_small_caps = False
-        self.markdown = Markdown()
-        self.translator = Translator("hl")
-        self.row_number = 0
+        # open window
         self.grid()
         self.top = self.winfo_toplevel()
-        self.top.state("zoomed")
+        self.top.state('zoomed')
         self.create_widgets()
+        self.heading.focus_set()
 
     def create_widgets(self):
         self.heading = Tk.Text(self, height=1, width=20, wrap=Tk.NONE)
         self.heading.grid(sticky=Tk.NE)
-        self.heading.bind("<Control-r>", self.refresh_random)
-        self.heading.bind("<Return>", self.bring_entry)
-        self.go_button = Tk.Button(self, text="GO!", command=self.bring_entry)
+        self.heading.bind('<Control-r>', self.refresh_random)
+        self.heading.bind('<Alt-d>', self.go_to_heading)
+        self.heading.bind('<Return>', self.bring_entry)
+        self.go_button = Tk.Button(self, text='GO!', command=self.bring_entry)
         self.go_button.grid(row=1, column=1, sticky=Tk.NW)
-        self.publish_button = Tk.Button(text="Publish", command=self.save)
+        self.publish_button = Tk.Button(text='Publish', command=self.save)
         self.publish_button.grid(row=1, column=2, sticky=Tk.NW)
         self.random_words = Tk.Label(self, textvariable=self.random_word)
         self.random_words.grid(row=1, column=0)
         self.edit_text = Tk.Text(self, height=27, width=88, font=('Courier New', '15'))
-        self.edit_text.bind("<Control-a>", self.select_all)
-        self.edit_text.bind("<Control-b>", self.bold)
-        self.edit_text.bind("<Control-f>", self.collapse_table)
-        self.edit_text.bind("<Control-i>", self.italic)
-        self.edit_text.bind("<Control-k>", self.small_caps)
-        self.edit_text.bind("<Control-n>", self.new_word)
-        self.edit_text.bind("<Control-r>", self.refresh_random)
-        self.edit_text.bind("<Control-s>", self.save)
-        self.edit_text.bind("<Control-t>", self.add_translation)
-        self.edit_text.bind("<Control-z>", self.bring_entry)
-        self.edit_text.bind("<Control-=>", self.add_definition)
-        self.edit_text.bind("<Control-BackSpace>", self.delete_word)
-        self.edit_text.bind("<Alt-d>", self.go_to_heading)
+        self.edit_text.bind('<Control-a>', self.select_all)
+        self.edit_text.bind('<Control-b>', self.bold)
+        self.edit_text.bind('<Control-i>', self.italic)
+        self.edit_text.bind('<Control-k>', self.small_caps)
+        self.edit_text.bind('<Control-n>', self.new_word)
+        self.edit_text.bind('<Control-r>', self.refresh_random)
+        self.edit_text.bind('<Control-s>', self.save)
+        self.edit_text.bind('<Control-t>', self.add_translation)
+        self.edit_text.bind('<Control-z>', self.bring_entry)
+        self.edit_text.bind('<Control-=>', self.add_definition)
+        self.edit_text.bind('<Control-BackSpace>', self.delete_word)
+        self.edit_text.bind('<Alt-d>', self.go_to_heading)
         self.edit_text.grid(row=1, rowspan=200, column=1)
         self.high_lulani.grid(row=2, column=0)
         self.english.grid(row=3, column=0)
-        self.heading.focus_set()
 
     def change_language(self, event=None):
         """
@@ -80,7 +84,7 @@ class EditDictionary(Tk.Frame):
         Select all the text in the edit area
         """
         self.edit_text.tag_add('sel', '1.0', 'end')
-        return "break"
+        return 'break'
 
     def delete_word(self, event):
         if self.edit_text.get(Tk.INSERT + "-1c") in '.,;:?!':
@@ -98,7 +102,7 @@ class EditDictionary(Tk.Frame):
         """
         self.heading.focus_set()
         self.heading.tag_add('sel', '1.0', 'end-1c')
-        return "break"
+        return 'break'
 
     def new_word(self, event=None):
         """
@@ -113,15 +117,15 @@ class EditDictionary(Tk.Frame):
                         '[6]\n').format(self.entry, trans.name, trans.convert_word(self.entry))
         self.edit_text.insert(1.0, new_template)
         self.edit_text.mark_set(Tk.INSERT, "4.7")
-        return "break"
+        return 'break'
 
     def add_definition(self, event=None):
         self.edit_text.insert(Tk.INSERT, "<div ==></div>")
         self.edit_text.mark_set(Tk.INSERT, Tk.INSERT + "-6c")
-        return "break"
         """
         Insert the markdown for entry definition, and move the insertion pointer to allow for immediate input of the definition.
         """
+        return 'break'
 
     def refresh_random(self, event=None):
         text = "\n".join(Translation.make_word(10))
@@ -164,7 +168,6 @@ class EditDictionary(Tk.Frame):
         try:
             text = self.edit_text.get(Tk.SEL_FIRST, Tk.SEL_LAST)
         except Tk.TclError:
-            return "break"
         if self.edit_text.compare(Tk.SEL_LAST, "==", Tk.SEL_LAST + " lineend"):
             enter = True
         re.sub(r'(<|href).*?>|\[.*?\]', '', text)
@@ -175,7 +178,6 @@ class EditDictionary(Tk.Frame):
         text += "\n" if enter else " "
         self.edit_text.insert(Tk.SEL_LAST + "+1c", text)
         self.edit_text.mark_set(Tk.INSERT, Tk.INSERT + "+" + str(len(text) + 1) + "c")
-        return "break"
 
     def collapse_table(self, event):
         text = self.edit_text.get(Tk.SEL_FIRST, Tk.SEL_LAST)
@@ -183,7 +185,7 @@ class EditDictionary(Tk.Frame):
         text = text.replace(' | [r]', '\n[r]')
         self.edit_text.delete(Tk.SEL_FIRST, Tk.SEL_LAST)
         self.edit_text.insert(Tk.INSERT, text)
-        return "break"
+        return 'break'
 
     def bring_entry(self, event=None):
         """
@@ -205,8 +207,8 @@ class EditDictionary(Tk.Frame):
         self.edit_text.delete(1.0, Tk.END)
         self.edit_text.insert(1.0, content)
         self.edit_text.focus_set()
-        self.edit_text.mark_set(Tk.INSERT, "1.0")
-        return "break"
+        self.edit_text.mark_set(Tk.INSERT, '1.0')
+        return 'break'
 
     def save(self, event=None):
         self.is_bold = self.is_italic = self.is_small_caps = False
@@ -228,8 +230,6 @@ class EditDictionary(Tk.Frame):
             self.page.remove()
         else:
             self.page.publish()
-        self.page.parent.publish()
-        self.page.root().publish()
         page = str(self.site)
         if page:
             with open(self.outputfile, 'w') as data:
@@ -237,14 +237,12 @@ class EditDictionary(Tk.Frame):
         page = str(self.dictionary.analyse())
         if page:
                 data.write(page)
-        return "break"
         text = str(self.site.analyse())
         if text:
             with open(self.searchfile, 'w') as data:
                 data.write(text)
         return 'break'
 
-app.master.title('Dictionary Edit')
 app = EditDictionary(dir='C:/Users/Ryan/Documents/TinellbianLanguages/dictionary',
                     outputfile='data.txt',
                     site=Dictionary(),
