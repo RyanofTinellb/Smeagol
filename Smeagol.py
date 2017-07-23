c6c109209d0bf58b9d0037510ea7d7d5a9a0b0a5
Duluri\Ryan <ryanoftinellb@gmail.com>
2017-07-23 20:21:15 +1000

    Search json does not see spans.

--- Smeagol.py
+++ Smeagol.py
@@ -815,7 +815,7 @@ class Page:
         wordlist = {}
         content = self.content[2:]
         """remove tags, and items between some tags"""
-        content = re.sub(r'\[\d\]|<(ipa|high-lulani)>.*?</\1>|<.*?>', ' ', content)
+        content = re.sub(r'\[\d\]|<(ipa|high-lulani|span)>.*?</\1>|<.*?>', ' ', content)
         """change punctuation to paragraph marks, so that splitlines works"""
         content = re.sub(r'[!?.]', '\n', content)
         """change punctuation to space"""
