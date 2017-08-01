1f27a3c9658332a13ae973f83ce56409fe73be08
Duluri\Ryan <ryanoftinellb@gmail.com>
2017-08-01 15:33:10 +1000

    Added methods to insert characters into text boxes, including formatting tags.

--- Edit.py
+++ Edit.py
@@ -36,18 +36,30 @@ class Edit(Tk.Frame):
         self.datafile = self.choose(kind, datafiles)
         self.site = self.choose(kind, sites)
         self.markdown = self.choose(kind, markdowns)
-        # create widgets
+        # create widgets and window
         self.headings = self.create_headings(self.buttonframe, widgets.number_of_headings)
         self.radios = self.create_radios(self.buttonframe, widgets.number_of_radios)
         self.blanklabel = Tk.Label(self.buttonframe, height=1000) # enough height to push all other widgets to the top of the window.
         self.infolabel = self.create_label(self.buttonframe)
         self.buttons = self.create_buttons(self.buttonframe)
-        self.textboxes = self.create_textboxes(self.textframe, widgets.number_of_textboxes)
+        commands = self.textbox_commands()
+        self.textboxes = self.create_textboxes(self.textframe, widgets.number_of_textboxes, commands)
         self.create_window()
         self.top = self.winfo_toplevel()
         self.top.state('zoomed')
 
+    def textbox_commands(self):
+        return [('<Control-b>', self.bold),
+        ('<Control-i>', self.italic),
+        ('<Control-k>', self.small_caps),
+        ('<KeyPress-|>', self.insert_pipe)]
+
     def create_window(self):
+        """
+        Place all widgets in GUI window.
+        Stack the textboxes on the right-hand side, taking as much room as possible.
+        Stack the heading boxes, the buttons, radiobuttons and a label in the top-left corner.
+        """
         self.pack(expand=True, fill=Tk.BOTH)
         self.buttonframe.pack(side=Tk.LEFT)
         self.textframe.pack(side=Tk.LEFT, expand=True, fill=Tk.BOTH)
@@ -69,10 +81,14 @@ class Edit(Tk.Frame):
         self.blanklabel.grid(row=row+1, column=0, columnspan=2)
 
     @staticmethod
-    def create_headings(master, number):
+    def create_headings(master, number, commands=None):
+        if not commands:
+            commands = []
         headings = []
         for _ in range(number):
             heading = Tk.Entry(master)
+            for (key, command) in commands:
+                heading.bind(key, command)
             headings.append(heading)
         return headings
 
@@ -93,10 +109,14 @@ class Edit(Tk.Frame):
         return radios
 
     @staticmethod
-    def create_textboxes(master, number):
+    def create_textboxes(master, number, commands=None):
+        if not commands:
+            commands = []
         textboxes = []
         for _ in range(number):
             textbox = Tk.Text(master, height=1, width=1)
+            for (key, command) in commands:
+                textbox.bind(key, command)
             textboxes.append(textbox)
         return textboxes
 
@@ -117,6 +137,60 @@ class Edit(Tk.Frame):
         except (TypeError, AttributeError, ValueError, KeyError):
             return variables
 
+    def insert_characters(self, event, before, after=''):
+        """
+        Insert given text into a Text textbox, either around an insertion cursor or selected text, and move the cursor to the appropriate place.
+        :param textbox (Tkinter Text): The Text into which the given text is to be inserted.
+        :param before (str): The text to be inserted before the insertion counter, or before the selected text.
+        :param after (str): The text to be inserted after the insertion cursor, or after the selected text.
+        """
+        textbox = event.widget
+        try:
+            text = textbox.get(Tk.SEL_FIRST, Tk.SEL_LAST)
+            textbox.delete(Tk.SEL_FIRST, Tk.SEL_LAST)
+            textbox.insert(Tk.INSERT, before + text + after)
+        except Tk.TclError:
+            textbox.insert(Tk.INSERT, before + after)
+            textbox.mark_set(Tk.INSERT, Tk.INSERT + '-{0}c'.format(len(after)))
+
+    def bold(self, event=None):
+        """
+        Insert markdown for bold tags, and place insertion point between them.
+        """
+        self.insert_characters(event, *self.find_formatting('strong'))
+        return 'break'
+
+    def italic(self, event=None):
+        """
+        Insert markdown for italic tags, and place insertion point between them.
+        """
+        self.insert_characters(event, *self.find_formatting('em'))
+        return 'break'
+
+    def small_caps(self, event=None):
+        """
+        Insert markdown for small-caps tags, and place insertion point between them.
+        """
+        self.insert_characters(event, *self.find_formatting('small-caps'))
+        return 'break'
+
+    def insert_pipe(self, event=None):
+        self.insert_characters(event, ' | ')
+        return 'break'
+
+    def find_formatting(self, keyword):
+        """
+        Find markdown for specific formatting.
+        :param keyword (str): the formatting type, in html, e.g.: strong, em, &c, &c.
+        :return (tuple): the opening and closing tags, in markdown, e.g.: ([[, ]]), (<<, >>)
+        """
+        if self.markdown:
+            start = self.markdown.markdown[self.markdown.markup.index('<' + keyword + '>')]
+            end = self.markdown.markdown[self.markdown.markup.index('</' + keyword + '>')]
+            return start, end
+        else:
+            return '', ''
+
     def load(self):
         """
         Find entry, manipulate entry to fit box, place in box.
@@ -131,7 +205,6 @@ class Edit(Tk.Frame):
         """
         text = self.edit_text.get(1.0, Tk.END + '-1c')
         self.entry.content = manipulate_entry_for_datafile(text)
-
 
     @staticmethod
     def manipulate_entry_for_textbox(text):
@@ -155,5 +228,6 @@ class Widgets():
 
 if __name__ == '__main__':
     widgets = Widgets(3, 3,  3)
-    app = Edit(widgets=widgets)
+    markdown = Translation.Markdown('c:/users/ryan/documents/tinellbianlanguages/dictionaryreplacements.html')
+    app = Edit(widgets=widgets, markdowns=markdown)
     app.mainloop()
