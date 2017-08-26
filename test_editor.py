db574e7a37c01a0587e933c51479451951f16c23
Duluri\Ryan <ryanoftinellb@gmail.com>
2017-08-26 14:22:50 +1000

    Add test for Editor

---
+++ test_editor.py
@@ -0,0 +1,29 @@
+import unittest
+import editor
+from translation import Markdown, AddRemoveLinks, ExternalDictionary
+from smeagol import Grammar
+
+class testEditorConfiguration(unittest.TestCase):
+    def setUp(self):
+        markdown = Markdown('c:/users/ryan/documents/tinellbianlanguages/'
+                                            'grammarstoryreplacements.mkd')
+        links = AddRemoveLinks([ExternalDictionary()])
+        self.editor = editor.Editor(site=Grammar(), markdown=markdown,
+                links=links)
+
+    def testEditorConfiguration(self):
+        expected = """destination: c:/users/ryan/documents/tinellbianlanguages/grammar
+name: Grammar
+source: data.txt
+template: template.html
+main_template: main_template.html
+markdown: ../replacements.mkd
+searchjson: searching.json
+leaf_level: 3
+externaldictionary\n"""
+        actual = self.editor.editor_configuration
+        self.assertEqual(expected, actual,
+                '\n\nExpected:\n{0}\nActual:\n{1}\n'.format(expected, actual))
+
+if __name__ == '__main__':
+    unittest.main()
