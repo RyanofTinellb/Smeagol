b3abec5cedd49a170705347bbb9dd5a959b23302
Duluri\Ryan <ryanoftinellb@gmail.com>
2017-09-05 18:31:40 +1000

    Rewrite flatname function to pass test

--- smeagol.py
+++ smeagol.py
@@ -957,28 +957,45 @@ class Page:
     def flatname(self):
         """
         'flattens' the name to only include letters aiu'pbtdcjkgmnqlrfsxh
-        minigolf scoring rules: smallest number wins.
+        Smallest number comes earlier in the ordering.
         """
-        name = self.name
         alphabet = "aiu'pbtdcjkgmnqlrfsxh"
-        radix = 2
+        punctuation = "-'#"
+        radix = len(punctuation)
+
+        name, score = urlform(self.name, self.markdown), 0
+
         double_letter = re.compile(r'([{0}])\1'.format(alphabet))
-        if re.match(r'\\-[aiu]', name):
-            name = name.replace(r'\-', "'", 1)
-            score = radix ** 15
-        elif name.startswith(r'\-'):
-            name = name.replace(r'\-', "", 1)
-            score = radix ** 15
-        else:
-            score = 0
-        while True:
-            try:
-                index = re.search(double_letter, name).start()
-            except AttributeError:
-                break
-            name = re.sub(double_letter, r'\1', name, 1)
-            score += radix ** index
+        name = re.sub(double_letter, r'\1#', name, 1)
+
+        for points, pattern in enumerate(punctuation):
+            score += score_pattern(name, pattern, radix, points+1)
+            name = name.replace(pattern, '*')
+
+        name = name.replace('*', '')
         return Flatname(name, score)
+
+def score_pattern(word, pattern, radix, points):
+    """
+    Calculate a score for each occurance of a pattern in the word.
+    More points are given for later occurances.
+
+    :param word: The word being tested.
+    :param pattern: The pattern being searched for.
+    :param points: The number of points given for this pattern.
+    """
+    total = sum([points * radix**index for index in pattern_indices(word, pattern)])
+    return total
+
+def pattern_indices(word, pattern):
+    index = -1
+    while True:
+        try:
+            index = word.index(pattern, index+1)
+            yield index+1
+        except ValueError:
+            raise StopIteration
+
 
 class ContentsMode:
     def __init__(self):
