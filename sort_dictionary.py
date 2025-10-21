6cc82cda6db53bdcab3b73edac471cfbf72f6d09
Your Name <you@example.com>
2025-10-21 20:31:49 +1100

    Sort dictionary by Tinellbian collation order

--- sort_dictionary.py
+++ sort_dictionary.py
@@ -4,9 +4,11 @@ import random
 from smeagol.utilities import filesystem as fs
 from smeagol.utilities.tinellbian_sort import SerialNumberer
 
+s = SerialNumberer()
+
 filename = 'c:/users/ryan/tinellbianlanguages/dictionary/data/data.src'
 text = fs.load_yaml(filename)
 lex = text['entries']['children']['The Tinellbian Languages Dictionary']['children']['lex']['children']
-lex = dict(sorted(lex.items(), key=lambda x: [s.change(n) for n in x.split('.')]))
+lex = dict(sorted(lex.items(), key=lambda x: [s.change(n) for n in x[0].split('.')]))
 text['entries']['children']['The Tinellbian Languages Dictionary']['children']['lex']['children'] = lex
 fs.save_yaml(text, filename)
